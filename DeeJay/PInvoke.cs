f4d20fdca05dd7dee154c45a36bb4fd1ece05b41
sichii <despereaux@live.com>
2023-04-27 13:55:00 -0400

    added a controlled exit

---
+++ DeeJay/PInvoke.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.Marshalling;
+
+#pragma warning disable CS1591
+
+namespace DeeJay;
+
+public partial class PInvoke
+{
+    public enum ControlTypes
+    {
+        CTRL_C_EVENT = 0,
+        CTRL_BREAK_EVENT,
+        CTRL_CLOSE_EVENT,
+        CTRL_LOGOFF_EVENT = 5,
+        CTRL_SHUTDOWN_EVENT
+    }
+    
+    public delegate bool HandlerRoutine(ControlTypes ctrlType);
+    
+    [LibraryImport("Kernel32")] 
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static partial bool SetConsoleCtrlHandler(HandlerRoutine handler, [MarshalAs(UnmanagedType.Bool)] bool add);
+}
--- DeeJay/Program.cs
+++ DeeJay/Program.cs
@@ -1,49 +1,65 @@
-﻿using DeeJay;
-using DeeJay.Abstractions;
+﻿using DeeJay.Abstractions;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-Environment.SetEnvironmentVariable("DOTNET_ReadyToRun", "0");
+namespace DeeJay;
 
-var services = new ServiceCollection();
-
-// @formatter:off
-var builder = new ConfigurationBuilder()
-                    .SetBasePath(Directory.GetCurrentDirectory())
-                    .AddJsonFile("appsettings.json")
-                    #if DEBUG
-                    .AddJsonFile("appsettings.local.json")
-                    #else
-                    .AddJsonFile("appsettings.prod.json")
-                    #endif
-                    ;
-
-var configuration = builder.Build();
-// @formatter:on
-
-var startup = new Startup(configuration);
-var ctx = new CancellationTokenSource();
-services.AddSingleton(_ => ctx);
-
-startup.ConfigureServices(services);
-
-var provider = services.BuildServiceProvider();
-
-var hostedServices = provider.GetServices<IHostedService>();
-
-AppDomain.CurrentDomain.ProcessExit += Cleanup;
-
-var tasks = hostedServices.Select(hs => hs.StartAsync(ctx.Token));
-
-await Task.WhenAll(tasks);
-await Task.Delay(-1);
-
-void Cleanup(object? sender, EventArgs e)
+internal class Program
 {
-    Console.WriteLine("Exiting...");
-    ctx.Cancel();
+    private static CancellationTokenSource? _ctx;
+    private static IGuildOptionsRepository? _guildOptionsRepo;
+    
+    public static async Task Main(string[] args)
+    {
+        Environment.SetEnvironmentVariable("DOTNET_ReadyToRun", "0");
 
-    var guildOptions = provider.GetRequiredService<IGuildOptionsRepository>();
-    guildOptions.SaveAsync().GetAwaiter().GetResult();
+        var services = new ServiceCollection();
+
+            // @formatter:off
+            var builder = new ConfigurationBuilder()
+                          .SetBasePath(Directory.GetCurrentDirectory())
+                          .AddJsonFile("appsettings.json")
+                          #if DEBUG
+                          .AddJsonFile("appsettings.local.json")
+                #else
+                    .AddJsonFile("appsettings.prod.json")
+                #endif
+                ;
+
+            var configuration = builder.Build();
+        // @formatter:on
+
+        var startup = new Startup(configuration);
+        var ctx = new CancellationTokenSource();
+        services.AddSingleton(_ => ctx);
+
+        startup.ConfigureServices(services);
+        var provider = services.BuildServiceProvider();
+        var hostedServices = provider.GetServices<IHostedService>();
+
+        SetupControlledExit(provider);
+        
+        var tasks = hostedServices.Select(hs => hs.StartAsync(ctx.Token));
+
+        await Task.WhenAll(tasks);
+        await Task.Delay(-1);
+    }
+
+    public static void SetupControlledExit(IServiceProvider provider)
+    {
+        _ctx = provider.GetRequiredService<CancellationTokenSource>();
+        _guildOptionsRepo = provider.GetRequiredService<IGuildOptionsRepository>();
+        var handler = new PInvoke.HandlerRoutine(InspectControlType);
+        PInvoke.SetConsoleCtrlHandler(handler, true);
+    }
+
+    public static bool InspectControlType(PInvoke.ControlTypes ctrlType)
+    {
+        Console.WriteLine("Exiting...");
+        _ctx!.Cancel();
+        _guildOptionsRepo!.SaveAsync().GetAwaiter().GetResult();
+
+        return true;
+    }
 }
